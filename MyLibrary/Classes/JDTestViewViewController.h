92a971f4ff1dfaa9e2009c742f4624a60468a0dc
lijin <18648250125@163.com>
2020-09-09 16:46:42 +0800

    first commit

---
+++ MyLibrary/Classes/JDTestViewViewController.h
@@ -0,0 +1,16 @@
+//
+//  JDTestViewViewController.h
+//  FBSnapshotTestCase
+//
+//  Created by 李晋 on 2020/9/8.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JDTestViewViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyLibrary/Classes/JDTestViewViewController.m
@@ -0,0 +1,31 @@
+//
+//  JDTestViewViewController.m
+//  FBSnapshotTestCase
+//
+//  Created by 李晋 on 2020/9/8.
+//
+
+#import "JDTestViewViewController.h"
+
+@interface JDTestViewViewController ()
+
+@end
+
+@implementation JDTestViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
